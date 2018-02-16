a421a181e7ce5e1d263b1057ee64c13fa8451a0a
Taiwo <andeladeveloper@Taiwo.local>
2018-02-16 13:07:04 +0100

    Implement base app

--- hotdog-not-hotdog/ViewController.swift
+++ hotdog-not-hotdog/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    let imagePicker = UIImagePickerController()
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = pickedImage
+
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("Failed to convert image to CIImage")
+            }
+
+            detect(image: ciImage)
+        }
+
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
+    func playSound(name: String) {
 
+        guard let sound = NSDataAsset(name: name) else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(data: sound.data, fileTypeHint: "m4a")
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    func detect(image: CIImage) {
+
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error laoding CoreML model.")
+        }
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to process image")
+            }
+
+            if let firstResult = results.first {
+
+                if firstResult.identifier.contains("hotdog") {
+
+                    self.navigationController?.navigationBar.barTintColor = UIColor(red:0.02, green:0.5, blue:0.09, alpha:1.0)
+                    self.navigationItem.title = "Hotdog!"
+                    self.playSound(name: "hotdog")
+                } else {
+
+                    self.navigationController?.navigationBar.barTintColor = UIColor(red:0.5, green:0.00, blue:0.00, alpha:1.0)
+                    self.navigationItem.title = "Not Hotdog!"
+                    self.playSound(name: "not-hotdog")
+                }
+            }
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+
+        do {
+            try handler.perform([request])
+        } catch {
+            print ("An error occurred performing the image analysis request")
+        }
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
